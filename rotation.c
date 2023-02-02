2d17165bc1c7be6c1edf039de518827f2d243beb
mrkayaalp <burakkayaalp52@gmail.com>
2023-02-02 14:24:54 +0300

    right_rotation fonk.

--- rotation.c
+++ rotation.c
@@ -1,9 +1,24 @@
 #include<stdio.h>
 
+void right_rotation(int matris[][], int n){
+    int n;
+    int temp[100][100];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            temp[i][j] = matris[n-j-i][i]
+        }
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            matris[i][j] = temp[i][j];
+        }
+}
+matris[j][n-i-1]
+
 
 int main(){
 
 
 
-    
+
 }
