85adf11ed1cc03d8b10b928345be2a53eac91694
mrkayaalp <burakkayaalp52@gmail.com>
2023-02-02 19:11:24 +0300

    matrisi kullanıcıdan isteme

--- rotation.c
+++ rotation.c
@@ -29,6 +29,27 @@ void left_rotation(int matris[][], int n){
     }
 }
 int main(){
+    int n;
+    int matris[100][100];
+    printf("Matrisin boyutunu (n) giriniz: ");
+    scanf("%d", &n);
+    printf("Matrisin elemanlarini giriniz: \n");
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            scanf("%d", &matris[i][j]); 
+        }
+    }
+
+    for(int i=0; i<n; i++){
+        printf("[")
+        for(int j=0; j<n; j++){
+            printf("%d", matris[i][j]); 
+        }
+        printf("]");
+    }
+
+
+    
 
 
 
